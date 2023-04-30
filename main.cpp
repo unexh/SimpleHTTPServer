3cc9ae0820492eb87967c5b81d183a3a186aa41f
Nishant Pandey <unexme21@gmail.com>
2023-04-30 18:35:44 +0530

    Removed references

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
 #include "iostream"
 #include "http_tcpServer_linux.h"
 
-/*
- * https://osasazamegbe.medium.com/showing-building-an-http-server-from-scratch-in-c-2da7c0db6cb7
- * https://github.com/OsasAzamegbe/http-server/blob/main/http_tcpServer_linux.cpp
- * https://www.youtube.com/watch?v=Lbfe3-v7yE0
- */
-
 int main() {
     using namespace http;
     std::cout << "Hello World \n";
